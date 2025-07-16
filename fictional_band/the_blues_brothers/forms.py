92f296d2c30628f0d03ab5f3b46155d4e7684fe8
lungelo01 <69298406+lungelo01@users.noreply.github.com>
2025-07-17 00:51:26 +0200

    Add docstrings to fictional_band/the_blues_brothers/forms.py

--- fictional_band/the_blues_brothers/forms.py
+++ fictional_band/the_blues_brothers/forms.py
@@ -3,6 +3,7 @@ from .models import Member
 
 
 class ContactForm(forms.ModelForm):
+    """Class contains contact forms about details of members."""
     class Meta:
         model = Member
         fields = ['firstname', 'lastname', 'message']
