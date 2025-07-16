a57a3f78fc486ae505060e85ef599588ea433fca
lungelo01 <69298406+lungelo01@users.noreply.github.com>
2025-07-17 00:56:20 +0200

    Add docstrings to fictional_band/the_blues_brothers/models.py

--- fictional_band/the_blues_brothers/models.py
+++ fictional_band/the_blues_brothers/models.py
@@ -3,10 +3,12 @@ from django.db import models
 
 # Create your models here.
 class Member(models.Model):
+    """The project model with firstname, lastname, message and date data and returns firstname and lastname."""
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
     date = models.DateField()
     message = models.CharField(max_length=255)
     
     def __str__(self):
+        """Returns the firstname and lastname"""
         return f"{self.firstname} {self.lastname}"
